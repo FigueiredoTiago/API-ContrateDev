ef00acb069259749d9cdce58ac4c210ff10c6b03
TFscript <developertiagof@gmail.com>
2025-06-20 19:09:32 -0300

    bd(alt na model do bd)

---
+++ prisma/migrations/20250620215256_add_profilecv/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "ProfileCv" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "githubUrl" TEXT NOT NULL,
+    "linkedinUrl" TEXT NOT NULL,
+    "websiteUrl" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "about" TEXT NOT NULL,
+    "stacks" TEXT[],
+
+    CONSTRAINT "ProfileCv_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProfileCv_userId_key" ON "ProfileCv"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProfileCv_email_key" ON "ProfileCv"("email");
+
+-- AddForeignKey
+ALTER TABLE "ProfileCv" ADD CONSTRAINT "ProfileCv_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
